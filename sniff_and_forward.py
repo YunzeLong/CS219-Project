13fd7488f75c56a8012fb54972f0be1556b8dd12
jerrylong <jerrylong@g.ucla.edu>
2023-06-05 03:26:25 -0700

    Added UDP support and incorporated forwarding logic.

--- sniff_and_forward.py
+++ sniff_and_forward.py
@@ -2,8 +2,24 @@ from scapy.all import *
 import time
 import argparse
 
+def copy_and_reroute(packet, new_dest_port):
+    if TCP in packet:
 
-def sniffer_and_filter(packet):
+        new_packet = packet.copy()
+        new_packet.dport = new_dest_port
+        new_packet.sport = get_self_port
+
+    elif UDP in packet:
+        new_packet = packet.copy()
+        new_packet.dport = new_dest_port
+    
+    else: # currently only support TCP and UDP
+        return None
+
+    return new_packet
+
+
+def sniffer_and_filter(packet,lora_IP, lora_port):
     # print(packet)
     if TCP in packet:
         if IP in packet:
@@ -25,8 +41,9 @@ def sniffer_and_filter(packet):
 
 
         # send here
-
-
+        packet_to_send = copy_and_reroute(packet, lora_port)
+        packet_to_send[IP].dst = lora_IP
+        send(packet_to_send)
 
     # let's say we focus on TCP packets for now.
 
@@ -43,7 +60,7 @@ def sniffer_and_filter(packet):
 
 if __name__ == '__main__':
 
-    source_port, destination_port, destination_ip = 0,0, ""
+    source_port, lora_port, lora_ip = 0,0, ""
 
     # init parser
     parser = argparse.ArgumentParser(
@@ -53,29 +70,29 @@ if __name__ == '__main__':
 
     parser.add_argument('-sport ','--srcport', dest='src_port', action='store',
                     default = 79,
-                    help='specify the source port to listen on')
+                    help='specify the source port (Middle box) to listen on')
 
     parser.add_argument('-dport ','--dstport', dest='dst_port', action='store',
                 default = 1700,
-                help='specify the destination port to forward to')
+                help='specify the destination (LoRa) port to forward to')
 
     parser.add_argument('-dip ','--dstIP', dest='dst_IP', action='store',
                 default = "192.168.1.100",
-                help='specify the destination IP to forward to')
+                help='specify the destination (LoRa) IP  to forward to')
 
     args = parser.parse_args()
 
     source_port = args.src_port
-    destination_port = args.dst_port
-    destination_ip = args.dst_IP
+    lora_port = args.dst_port
+    lora_ip = args.dst_IP
     # end parsing
 
 
-    print(f"src port: {source_port}, dst port: {destination_port}, dst ip: {destination_ip}")
+    print(f"src port: {source_port}, dst port: {lora_port}, dst ip: {lora_ip}")
 
     # sniff for UL traffic
 
     # filter on port
-    sniff(filter = f'src port {source_port}', prn=sniffer_and_filter)
+    sniff(filter = f'src port {source_port}', prn=lambda pkt: sniffer_and_filter(pkt, lora_ip, lora_port))
     # sniff(prn=sniffer_and_filter)
 
